50bd229e2da9623922553f5407d903f2ebd6c2f7
bradpresleyk <bradpresley30@gmail.com>
2024-03-27 09:19:42 -0700

    compiled code

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,13 +7,14 @@
  */
 int check_cycle(listint_t *list)
 {
+
+	listint_t *slow = list;
+	listint_t *fast = list;
+
 	if (list == NULL)
 	{
 	return (0);
 	}
-
-	listint_t *slow = list;
-	listint_t *fast = list;
 
 	while (slow != NULL && fast != NULL && fast->next != NULL)
 	{
